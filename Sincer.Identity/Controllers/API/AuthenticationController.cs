d3888ebcb5ebe249d9ecf9a22cc4e9bd7215069b
Jaco Jansen van Vuuren <jacojvv.hello@gmail.com>
2019-06-05 19:53:02 +0200

    Add project files.

---
+++ Sincer.Identity/Controllers/API/AuthenticationController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sincer.Identity.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private UserManager<IdentityUser> _userManager;
+        private SignInManager<IdentityUser> _signInManager;
+
+        public AuthenticationController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register(AuthenticationRequest request)
+        {
+            var user = new IdentityUser { UserName = request.Email, Email = request.Email };
+            var result = await _userManager.CreateAsync(user, request.Password);
+
+            if (result.Succeeded)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }           
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(AuthenticationRequest request)
+        {
+            var result = await _signInManager.PasswordSignInAsync(request.Email, request.Password, false, true);
+            if (result.Succeeded)
+            {
+                return NoContent();
+            }
+
+            return BadRequest();
+        }
+    }
+
+    public class AuthenticationRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
